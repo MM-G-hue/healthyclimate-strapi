ae034b7c0f34f47db26e739694b98c1e4df9483a
MM-G-hue <marijn.martin@hotmail.com>
2024-10-07 10:47:24 +0200

    update .env file

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('STRAPI_DATABASE_CLIENT', 'sqlite');
 
   const connections = {
     mysql: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        host: env('STRAPI_DATABASE_HOST', 'localhost'),
+        port: env.int('STRAPI_DATABASE_PORT', 3306),
+        database: env('STRAPI_DATABASE_NAME', 'strapi'),
+        user: env('STRAPI_DATABASE_USERNAME', 'strapi'),
+        password: env('STRAPI_DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('STRAPI_DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
@@ -25,12 +25,12 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        host: env('STRAPI_DATABASE_HOST', 'localhost'),
+        port: env.int('STRAPI_DATABASE_PORT', 5432),
+        database: env('STRAPI_DATABASE_NAME', 'strapi'),
+        user: env('STRAPI_DATABASE_USERNAME', 'strapi'),
+        password: env('STRAPI_DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('STRAPI_DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
@@ -44,7 +44,7 @@ module.exports = ({ env }) => {
     },
     sqlite: {
       connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+        filename: path.join(__dirname, '..', env('STRAPI_DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true,
     },
